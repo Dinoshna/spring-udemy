d1c923b773dcc413927b98fd40ad532d17885942
Dinoshna Thathuru <dinulearnings@gmail.com>
2024-04-21 18:33:35 +0530

    csrf disabled and security config

--- src/main/java/com/learning/springsecurity/config/SpringSecurityConfig.java
+++ src/main/java/com/learning/springsecurity/config/SpringSecurityConfig.java
@@ -1,4 +1,25 @@
 package com.learning.springsecurity.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SpringSecurityConfig {
-}
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception{
+        security.csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(request-> request.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return security.build();
+    }
+
+    }
