fb9133086b2e8040972efbf6e0871c5640d7c4df
Dinoshna Thathuru <dinulearnings@gmail.com>
2024-04-21 17:53:05 +0530

    getting csrf code token in each session

--- src/main/java/com/learning/springsecurity/controller/StudentController.java
+++ src/main/java/com/learning/springsecurity/controller/StudentController.java
@@ -1,15 +1,29 @@
 package com.learning.springsecurity.controller;
 
+import com.learning.springsecurity.model.ExpenseDetails;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
+    List<ExpenseDetails> details = new ArrayList<>(List.of(
+            new ExpenseDetails(1, "shopping"),
+            new ExpenseDetails(2, "food")));
 
     @GetMapping("csrf-token")
     public CsrfToken generateCsrfToken(HttpServletRequest request) {
         return (CsrfToken) request.getAttribute("_csrf");
     }
+
+    @GetMapping("/expense-details")
+    public List<ExpenseDetails> getExpenseDetails() {
+        return details;
+
+    }
+
 }
