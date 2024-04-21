e220618c08f64cd8a4f17fe4013fea137f17cc3f
Dinoshna Thathuru <dinulearnings@gmail.com>
2024-04-21 17:33:49 +0530

    added model and lombok dependency with annotations

---
+++ src/main/java/com/learning/springsecurity/model/ExpenseDetails.java
@@ -0,0 +1,18 @@
+package com.learning.springsecurity.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExpenseDetails {
+
+    private int id;
+    private String name;
+//    private List<ArrayList> expenseList;
+
+}
